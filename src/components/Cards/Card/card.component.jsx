ad6927fc2d72322c85545ad2898f35f5cc3bb36c
Sarvar <jalolov.08@mail.ru>
2023-12-22 20:55:53 +0500

    edits in styles

--- src/components/Cards/Card/card.component.jsx
+++ src/components/Cards/Card/card.component.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import '../cards.style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarCheck, faCoins, faPercent } from '@fortawesome/free-solid-svg-icons';
 
 export default function Card({ data }) {
-    return (
-        <div className='card'>
-            <div className='card_logo_container'>
-                <img src={data.photo} className='card_logo' />
-            </div>
-            <div className='card_title_container'>
-                <p className='card_title'>{data.title}</p>
-            </div>
-            <div className='card_texts_container'>
-                <h1 className='card_up_to'>До {data.upTo.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} <br /> рублей </h1>
-                <p className='card_from'>от {data.from}% в день</p>
-                <p className='card_from'>от {data.minAge} до {data.maxAge} лет</p>
-                <a href={data.link} target='__blank'>
-                    <button className='card_btn'>
-                        Получить деньги
-                    </button>
-                </a>
-                <p className='card_tin'>Реклама.{data.advertising} ИНН {data.TIN}</p>
-            </div>
-
-        </div>
-    );
+  return (
+    <div className='card'>
+      <div className='card_logo_container'>
+        <img src={data.photo} className='card_logo' alt='Card Logo' />
+      </div>
+      <div className='card_title_container'>
+        <p className='card_title'>{data.title}</p>
+      </div>
+      <div className='card_texts_container'>
+        <h1 className='card_up_to'>
+          <FontAwesomeIcon icon={faPercent} className='card_icon' /> До{' '}
+          {data.upTo.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} <br /> рублей
+        </h1>
+        <p className='card_from'>
+          <FontAwesomeIcon icon={faCoins} className='card_icon' /> от {data.from}% в день
+        </p>
+        <p className='card_from'>
+          <FontAwesomeIcon icon={faCalendarCheck} className='card_icon' /> от {data.minAge} до {data.maxAge} лет
+        </p>
+        <a href={data.link} target='_blank' rel='noopener noreferrer'>
+          <button className='card_btn'>Получить деньги</button>
+        </a>
+        <p className='card_tin'>
+          Реклама.{data.advertising} ИНН {data.TIN}
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/Notice/notice.component.jsx
+++ src/components/Notice/notice.component.jsx
@@ -17,16 +17,16 @@ export default function Modal() {
         <div className='notice'>
           <img src={ezaem} className='notice_img' />
           <div className='notice_texts'>
-             <b className='notice_title'>Вам одобрят 30 000 под 0%!</b>
-         <a href='/'>
-         <p className='notice_desc'>
-              Жмите, чтобы получить деньги <br />
-              без отказов и проверок!
-            </p>
-         </a>
+            <b className='notice_title'>Вам одобрят 30 000 под 0%!</b>
+            <a href='/'>
+              <p className='notice_desc'>
+                Жмите, чтобы получить деньги <br />
+                без отказов и проверок!
+              </p>
+            </a>
             <p className='notice_tin'>Реклама. ООО МФК«Веритас». ИНН 7706780186</p>
           </div>
-            <FontAwesomeIcon icon={faTimes} className='close_icon' onClick={handleClose} />
+          <FontAwesomeIcon icon={faTimes} className='close_icon' onClick={handleClose} />
         </div>
       )}
     </>
